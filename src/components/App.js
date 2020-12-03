1eeecd369eec35248cb8985b5e21047192edce71
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-12-04 02:16:49 +0400

    Making pagination: useEffect attempt in App

--- src/components/App.js
+++ src/components/App.js
@@ -7,32 +7,40 @@ function App() {
   const [data, setData] = React.useState();
   const [paginationConfig, setPaginationConfig] = React.useState();
   const [dataPortion, setDataPortion] = React.useState();
+  const buttonRefLarge = React.useRef();
+  const buttonRefSmall = React.useRef();
 
-  async function handleClick(e) {
-    const res = await getData(e.target.id === 'largeSetButton' ? true : false);
-    setData(res);
-    console.log('data', data);
-    setPaginationConfig({
-      increment: 50,
-      currentStart: 0,
-      remainder: data.length,
-    });
-    console.log('paginationConfig', paginationConfig);
-    // const currentEnd = paginationConfig.currentStart + paginationConfig.increment;
-    // setDataPortion(data.slice(paginationConfig.currentStart, currentEnd));
-    // const nextStart = paginationConfig.currentStart += paginationConfig.increment;
-    // const nextRemainder = paginationConfig.remainder -= paginationConfig.increment;
-    // setPaginationConfig({
-    //   increment: 50,
-    //   currentStart: nextStart,
-    //   remainder: nextRemainder,
-    // });
-  }
+
+  React.useEffect(() => {
+    async function handleClick(e) {
+      const res = await getData(e.target.id === 'largeSetButton' ? true : false);
+      console.log('res', res);
+      setData(res);
+      console.log('data', data);
+      setPaginationConfig({
+        increment: 50,
+        currentStart: 0,
+        remainder: data.length,
+      });
+      console.log('paginationConfig', paginationConfig);
+      // const currentEnd = paginationConfig.currentStart + paginationConfig.increment;
+      // setDataPortion(data.slice(paginationConfig.currentStart, currentEnd));
+      // const nextStart = paginationConfig.currentStart += paginationConfig.increment;
+      // const nextRemainder = paginationConfig.remainder -= paginationConfig.increment;
+      // setPaginationConfig({
+      //   increment: 50,
+      //   currentStart: nextStart,
+      //   remainder: nextRemainder,
+      // });
+    }
+    buttonRefLarge.current.addEventListener('click', handleClick);
+    buttonRefSmall.current.addEventListener('click', handleClick);
+  }, []);
 
   return (
     <div className="App">
-      <button type='button' onClick={handleClick} id='largeSetButton' >Получить большой набор данных</button>
-      <button type='button' onClick={handleClick} id='smallSetButton' >Получить малый набор данных</button>
+      <button type='button' ref={buttonRefLarge} id='largeSetButton' >Получить большой набор данных</button>
+      <button type='button' ref={buttonRefSmall} id='smallSetButton' >Получить малый набор данных</button>
       <Table data={dataPortion} ></Table>
     </div>
   );
